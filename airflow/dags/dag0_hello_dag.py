a813d8e70eb6aaf922e97c2aa61fd2f96498e81e
Holland Brown <hrreagor.brown@gmail.com>
2025-10-23 11:02:15 -0400

    commit first airflow dag

--- airflow/dags/dag0_hello_dag.py
+++ airflow/dags/dag0_hello_dag.py
@@ -1,20 +1,6 @@
-# Hello DAG
-# initial DAG so UI shows something as I develop the infra
+# DAG 0: Hello DAG
 
-# from datetime import datetime
-# from airflow import DAG
-# from airflow.operators.empty import EmptyOperator
-
-# with DAG(
-#     dag_id="hello_dag",
-#     start_date=datetime.today(),
-#     schedule_interval=None,
-#     catchup=False,
-#     tags=["day0"],
-# ) as dag:
-#     start = EmptyOperator(task_id="start")
-#     end = EmptyOperator(task_id="end")
-#     start >> end
+# initial DAG so Airflow UI shows something as I develop the infra
 
 from __future__ import annotations
 

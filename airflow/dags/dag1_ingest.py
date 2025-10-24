d9b04af8ff68cab2f8335a3acfbb0401db453290
Holland Brown <hrreagor.brown@gmail.com>
2025-10-24 16:23:08 -0400

    org data into GCS bucket and bigquery dataset

--- airflow/dags/dag1_ingest.py
+++ airflow/dags/dag1_ingest.py
@@ -14,27 +14,33 @@
 # (FHIR: https://www.healthit.gov/sites/default/files/2019-08/ONCFHIRFSWhatIsFHIR.pdf)
 
 
-
+import time
 from airflow import DAG
-from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
-from airflow.operators.python import PythonOperator
+# from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+# from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryUpdateDatasetOperator
+# from airflow.providers.google.cloud.operators.bigquery import BigQueryUpsertTableOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
-import requests, json, os, pandas as pd
+import requests, json, os
+# from scripts.call_npi_api import fetch_npi_data
+import warnings
 
 GCS_BUCKET = os.getenv("GCS_BUCKET", "npi-bronze-bucket")
 BQ_DATASET = os.getenv("BIGQUERY_DATASET", "bronze")
 PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 
 def fetch_npi_data(**context):
-    url = "https://npiregistry.cms.hhs.gov/api/?version=2.1&state=CA&limit=10"
-    data = requests.get(url).json()
-    os.makedirs("/tmp/npi", exist_ok=True)
-    path = "/tmp/npi/npi_sample.json"
+    url = os.getenv("NPI_URL")
+    if url == None:
+        warnings.warn(f"\nWARNING!!! No NPPES NPI URL indicated!\n")
+    data = requests.get(url) #.json()
+    os.makedirs(f"{os.getcwd()}/tmp/npi", exist_ok=True)   # no error raised if exists
+    path = f"{os.getcwd()}/tmp/npi/npi_sample.json"
     with open(path, "w") as f:
         json.dump(data, f)
     context['ti'].xcom_push(key='npi_path', value=path)
-
+    
 with DAG(
     "npi_ingest",
     start_date=datetime(2025, 10, 23),
@@ -48,28 +54,47 @@ with DAG(
         python_callable=fetch_npi_data,
     )
 
-    upload = LocalFilesystemToGCSOperator(
-        task_id="upload_to_gcs",
-        src="{{ ti.xcom_pull(task_ids='fetch_npi_data', key='npi_path') }}",
-        dst="bronze/npi_sample.json",
-        bucket=GCS_BUCKET,
+    # upload = LocalFilesystemToGCSOperator(
+    #     task_id="upload_to_gcs",
+    #     src="{{ ti.xcom_pull(task_ids='fetch_npi_data', key='npi_path') }}",
+    #     dst="bronze/npi_sample.json",
+    #     bucket=GCS_BUCKET,
+    # )
+
+    # load_to_bq = BigQueryInsertJobOperator(
+    #     # Load data from GCS bucket to BQ dataset
+    #     task_id="load_to_bigquery",
+    #     configuration={
+    #         "load": {
+    #             "sourceUris": [f"gs://{GCS_BUCKET}/bronze/npi_sample.json"],
+    #             "destinationTable": {
+    #                 "projectId": PROJECT_ID,
+    #                 "datasetId": BQ_DATASET,
+    #                 "tableId": "npi_raw"
+    #             },
+    #             "sourceFormat": "NEWLINE_DELIMITED_JSON",
+    #             "autodetect": True,
+    #             "writeDisposition": "WRITE_TRUNCATE",
+    #         }
+    #     }
+    # )
+
+    update_dataset = BigQueryUpdateDatasetOperator(
+        task_id="update_dataset",
+        dataset_id=BQ_DATASET,
+        dataset_resource={"description": "Updated dataset"},
     )
 
-    load_to_bq = BigQueryInsertJobOperator(
-        task_id="load_to_bigquery",
-        configuration={
-            "load": {
-                "sourceUris": [f"gs://{GCS_BUCKET}/bronze/npi_sample.json"],
-                "destinationTable": {
-                    "projectId": PROJECT_ID,
-                    "datasetId": BQ_DATASET,
-                    "tableId": "npi_raw"
-                },
-                "sourceFormat": "NEWLINE_DELIMITED_JSON",
-                "autodetect": True,
-                "writeDisposition": "WRITE_TRUNCATE",
-            }
-        }
-    )
+    # # "upsert" table : update and insert; don't overwrite existing data
+    # upsert = BigQueryUpsertTableOperator(
+    #     task_id="upsert_table",
+    #     dataset_id=BQ_DATASET,
+    #     table_resource={
+    #         "tableReference": {"tableId": "test_table_id"},
+    #         "expirationTime": (int(time.time()) + 300) * 1000,
+    #     },
+    # )
 
-    fetch >> upload >> load_to_bq
+    # fetch >> upload >> load_to_bq
+    fetch >> update_dataset
+
